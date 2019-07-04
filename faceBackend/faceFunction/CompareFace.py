1ee1028dd37bb6e1ecf96a4b7748e31038efdfcb
sdtsztom <sdtsztom@163.com>
2019-07-04 11:55:12 +0800

    完成faceCheckCompare功能

--- faceBackend/faceFunction/CompareFace.py
+++ faceBackend/faceFunction/CompareFace.py
@@ -2,7 +2,7 @@ from ..faceLib import facelib as fl
 from ..faceLib import facedb as fdb
 import numpy as np
 
-def compareFaceDB(img,location=None):
+def faceSearchCompare(img, location=None):
     if not location:
         fFinder=fl.faceFinder();
         location=fFinder.findFaces(img)[0]
@@ -14,3 +14,17 @@ def compareFaceDB(img,location=None):
     CandidatesInfo=db.getPeopleInfoByIndexes(CandidatesIndex)
     info=[[CandidatesInfo[i][0],CandidatesInfo[i][1],distances[index]] for i,index in enumerate(CandidatesIndex)]   #info:[ID,name,distance]
     return info
+
+def faceCheckCompare(imgList):
+    fFinder = fl.faceFinder();
+    encodings=[0]*len(imgList)
+    for i,img in enumerate(imgList):
+        location=fFinder.findFaces(img)[0]
+        encodings[i]=fl.encodeFace(img, location)
+    flag_all_pass=True
+    for encoding in encodings:
+        res=fl.faceCompare(encodings,encoding)
+        flag_all_pass=np.all(res)
+        if not flag_all_pass:
+            break
+    return flag_all_pass
--- faceBackend/verification/FaceCompareChecker.py
+++ faceBackend/verification/FaceCompareChecker.py
@@ -2,5 +2,5 @@ import faceBackend as fb
 import cv2
 
 img=cv2.imread('../auxiliary/Tang2.jpg')
-info=fb.compareFaceDB(img)
+info=fb.faceSearchCompare(img)
 print(info)
